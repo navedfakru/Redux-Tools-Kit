db00939196cbc8fa4fc26fbd21dede3268f55343
Naved Ali <naved10055@gmail.com>
2024-07-19 22:44:39 +0530

    9 Remove to Cart UI

--- Redux/components/Product.js
+++ Redux/components/Product.js
@@ -1,14 +1,30 @@
 import { StyleSheet, Text, View, Image, Button } from 'react-native';
-import { addToCart } from './redux/action';
-import { useDispatch } from 'react-redux';
+import { addToCart, removeToCart } from './redux/action';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
 
 const Product = ({item}) => {
 
   const dispatch = useDispatch();
+  const cartItems = useSelector((state) => state.reducer);
+  const [isAdded, setIsAdded] = useState(false);
 
   const handleAddToCart = (item) => {
     dispatch(addToCart(item))
   }
+  const handleRemoveToCart = (item) => {
+    dispatch(removeToCart(item))
+  }
+
+  useEffect(()=> {
+    if(cartItems && cartItems.length){
+      cartItems.forEach((element) => {
+        if(element.name === item.name){
+          setIsAdded(true)
+        }
+      })
+    }
+  }, [cartItems])
 
   return (
     <View style={{padding: 20}}>
@@ -19,7 +35,13 @@ const Product = ({item}) => {
       style={{ width: 200, height: 200 }}
       source={{ uri: item.image }}
     />
-    <Button title="Add to Card" onPress={() => handleAddToCart(item)} />
+    {
+      isAdded ? 
+      <Button title="Remove to Card" onPress={() => handleRemoveToCart(item)} /> 
+      :
+      <Button title="Add to Card" onPress={() => handleAddToCart(item)} /> 
+    }
+    
   </View>
   );
 }
--- Redux/components/redux/action.js
+++ Redux/components/redux/action.js
@@ -1,8 +1,14 @@
-import { Add_To_Cart } from "./constants";
+import { Add_To_Cart, Remove_To_Cart } from "./constants";
 
 export function addToCart(itme) {
   return {
     type: Add_To_Cart,
+    data: itme
+  }
+}
+export function removeToCart(itme) {
+  return {
+    type: Remove_To_Cart,
     data: itme
   }
 }
